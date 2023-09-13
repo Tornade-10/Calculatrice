2770de322d2a4400c6a8a64c3e93bf56e2495a00
Tornado <144366439+Tornade-10@users.noreply.github.com>
2023-09-13 11:25:42 +0200

    Add files via upload

---
+++ Calculatrice-main/Calculatrice/Calculateur.cpp
@@ -0,0 +1,32 @@
+#include "Calculateur.h"
+#include <iostream>
+
+//instructions de l'addition
+int add(int a, int b)
+{
+	return a + b;
+}
+//instructions de la soustraction
+int substract(int a, int b)
+{
+	return a - b;
+}
+//instructions de la multiplication
+int multiply(int a, int b)
+{
+	return a * b;
+}
+//instructions de la division
+float divide(int a, int b)
+{
+	if (b != 0)
+	{
+		return a / b;
+	}
+	else
+	{
+		std::cout << "non \n";
+	}
+}
+
+
---
+++ Calculatrice-main/Calculatrice/Calculatrice.cpp
@@ -0,0 +1,42 @@
+// Calculatrice.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
+//
+
+#include <iostream>
+#include "Calculateur.h"
+
+int main()
+{
+	/*selection des variables */
+	int a;
+	int b;
+	char operation;
+
+	/*affichagge des variables et choix de leur values*/
+	std::cout << "selectionner deux nombres a puis b\n";
+	std::cin >> a;
+	std::cin >> b;
+
+	/*choix de l'operation*/
+	std::cout << "operation?" << "\n";
+	std::cin >> operation;
+	/*choix de l'operation en fonction du joueur*/
+	switch (operation)
+	{
+	case '+':
+		std::cout << "somme :\t" << add(a, b) << "\n";
+		break;
+
+	case '-':
+		std::cout << "difference :\t" << substract(a, b) << "\n";
+		break;
+
+	case '*':
+		std::cout << "produit :\t" << multiply(a, b) << "\n";
+		break;
+
+	case '/':
+		std::cout << "quotient :\t" << divide(a, b) << "\n";
+		break;
+
+	} 
+}
